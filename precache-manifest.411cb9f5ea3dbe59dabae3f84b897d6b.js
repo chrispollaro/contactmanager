b41ff27a3841958014ab92102841f68cd79f3399
Christopher Pollaro <chris@chrispollaro.com>
2018-11-30 11:28:56 -0500

    Updates

---
+++ precache-manifest.411cb9f5ea3dbe59dabae3f84b897d6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "63aff9f71ea114de417b",
+    "url": "/contactmanager/static/js/main.63aff9f7.chunk.js"
+  },
+  {
+    "revision": "2909a915932c9ebeb406",
+    "url": "/contactmanager/static/js/1.2909a915.chunk.js"
+  },
+  {
+    "revision": "63aff9f71ea114de417b",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "2909a915932c9ebeb406",
+    "url": "/contactmanager/static/css/1.a840bea0.chunk.css"
+  },
+  {
+    "revision": "6296abc72400a63cb4ad4eaeb4a06ca7",
+    "url": "/contactmanager/index.html"
+  }
+];
